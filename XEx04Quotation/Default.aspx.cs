a33b1c55db8ec196d9296714e812ba02dfcb166e
robinsonda615@outlook.com <robinsonda615@outlook.com>
2020-02-15 14:29:03 -0600

    Added  Quote Class Session State to Default Form
    Added a Quote Class to the Models folder to contain
    the variables of each quote.
    Recorded the Quote object to Session State in the Default form.

--- XEx04Quotation/Default.aspx.cs
+++ XEx04Quotation/Default.aspx.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -9,6 +10,8 @@ namespace XEx04Quotation
 {
     public partial class Default : System.Web.UI.Page
     {
+        private Quote quote;
+
         protected void Page_Load(object sender, EventArgs e)
         {
             UnobtrusiveValidationMode = UnobtrusiveValidationMode.None;
@@ -20,17 +23,27 @@ namespace XEx04Quotation
             {
                 decimal salesPrice = Convert.ToDecimal(txtSalesPrice.Text);
                 decimal discountPercent = Convert.ToDecimal(txtDiscountPercent.Text) / 100;
-
                 decimal discountAmount = salesPrice * discountPercent;
                 decimal totalPrice = salesPrice - discountAmount;
 
                 lblDiscountAmount.Text = discountAmount.ToString("c");
                 lblTotalPrice.Text = totalPrice.ToString("c");
+
+                quote.SalesPrice = salesPrice;
+                quote.DiscountAmount = discountAmount;
+                quote.TotalPrice = totalPrice;
+
+                HttpContext.Current.Session["Quotation"] = quote;
+               
             }   
         }
 
         protected void btnConfirm_Click(object sender, EventArgs e)
         {
+            if (IsValid)
+            {
+
+            }
 
         }
     }
---
+++ XEx04Quotation/Models/Quote.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+
+    public class Quote
+    {
+       public decimal SalesPrice { get; set; }
+       public decimal DiscountAmount { get; set; }
+       public decimal TotalPrice { get; set; }
+
+        //method to get Quote object from session state
+        //public static Quote GetQuote()
+        //{
+        //Quote quote = (Quote)HttpContext.Current.Session["Quotation"];
+        //if (quote == null)
+        //    HttpContext.Current.Session["Quotation"] = new Quote();
+        //return (Quote)HttpContext.Current.Session["Quotation"];
+        //}
+    }
